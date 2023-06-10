92223dc73dc3444a6d3b680c00e0ca67d695428e
Allen <124841882@qq.com>
2023-06-10 21:19:33 +0800

    20230610-2118

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="app">
+        <el-table>
+            <el-table-column type="selection" width="150">选择</el-table-column>
+            <el-table-column prop="name" label="姓名">
+                <template slot-scope="scope">
+                    {{ scope.row.name }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="age" label="年龄">
+                <template slot-scope="scope">
+                    {{ scope.row.age }}
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script setup>
+// import Edit from './components/Edit.vue';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const list = ref([]);
+const getList = async () => {
+    // 接口调用
+    const res = await axios.get('/list');
+
+    list.value = res.data;
+};
+
+onMounted(() => {
+    getList();
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Son.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>{{ cc }}</div>
+    <h1>{{ count }}</h1>
+    <button @click="handleClick">Button Add</button>
+</template>
+
+<script setup name="son">
+import { inject, reactive, ref } from 'vue';
+
+const cc = inject('name');
+let count = inject('data');
+
+const name = ref('rock');
+const age = ref(18);
+
+const handleClick = () => {
+    count.value += 1;
+};
+
+const obj = reactive({
+    x: 18,
+    y: 20
+});
+
+const aa = () => {
+    console.log('aa executed');
+    // return 0;
+};
+
+defineExpose({ obj, name, age, aa });
+</script>
+
+<style lang="scss" scoped></style>
