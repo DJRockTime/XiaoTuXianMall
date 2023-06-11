c6094b7f12fb44600fc46f86cf5033b81e316a91
DJRockTime <38347978+DJRockTime@users.noreply.github.com>
2023-06-11 20:23:30 +0800

    20230611-2015

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,26 +1,26 @@
-import Mock from 'mockjs';
+import Mock from "mockjs";
 
 const arr = [];
 
 for (let i = 0; i < 10; i++) {
     arr.push({
-        id: Mock.mock('@guid'),
-        name: Mock.mock('@cname'),
-        age: Mock.mock('@integer(1, 100)')
+        id: Mock.mock("@guid"),
+        name: Mock.mock("@cname"),
+        age: Mock.mock("@integer(1, 100)")
     });
 }
 
 export default [
     {
-        url: '/list',
-        method: 'get',
+        url: "/list",
+        method: "get",
         response: () => {
             return arr;
         }
     },
     {
-        url: 'de/:id',
-        method: 'delete',
+        url: "de/:id",
+        method: "delete",
         response: () => {
             const index = arr.findIndex((item) => item.id === req.query.id);
             if (index > -1) {
