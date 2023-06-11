2311bf8b8ee24201c7eecc55a6e1f929137648a5
DJRockTime <38347978+DJRockTime@users.noreply.github.com>
2023-06-11 23:40:12 +0800

    20230611-2340

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-        hello world
-    </div>
+    <div>hello world</div>
+    <h1>{{ counterStore.count }}</h1>
+    <button @click="counterStore.increment">Button Add</button>
 </template>
 
 <script setup>
+import { useCounterStore } from "./stores/counters";
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
 
+const counterStore = useCounterStore();
+console.log(counterStore);
+
+// 响应式数据需要storeToRefs来解构
+const { count, double } = storeToRefs(counterStore);
+
+// 方法需要单独结构
+const { increment } = counterStore;
+
+onMounted(() => {
+    counterStore.getList();
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
