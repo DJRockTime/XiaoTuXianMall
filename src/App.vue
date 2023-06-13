1609e6e14d913b030248b3f6d6da7384a80d583f
DJRockTime <38347978+DJRockTime@users.noreply.github.com>
2023-06-14 00:33:48 +0800

    20230614-0033

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-<<<<<<< HEAD
-    <div>hello world</div>
-=======
     <el-row class="mb-4">
         <el-button type="primary">Main</el-button>
     </el-row>
->>>>>>> dev2023/06/12
 </template>
 
 <script setup></script>
